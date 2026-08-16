87f89c549c749e75765975d48ec3ef3e6186dc2a
Ravi Mahajan <ravidnya@gmail.com>
2026-08-16 19:32:32 -0400

    Fix Netlify CORS with relative proxy and _redirects

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,38 +1,11 @@
 export class ApiConstants {
   public static readonly API_KEY = 'rc_live_d80a3f2292cd46dcba060e5467fa776e';
 
-  private static getBaseHost(): string {
-    if (typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')) {
-      return '';
-    }
-    return 'https://api.restcountries.com';
-  }
-
-  public static get BASE_URL(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5`;
-  }
-
-  public static get GET_ALL_COUNTRIES(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5`;
-  }
-
-  public static get COUNTRIES_BY_REGION(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5/region/`;
-  }
-
-  public static get COUNTRY_DATA(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5/codes.alpha_2/`;
-  }
-
-  public static get COUNTRY_BY_ALPHA2(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5/codes.alpha_2/`;
-  }
-
-  public static get COUNTRY_BY_ALPHA3(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5/codes.alpha_3/`;
-  }
-
-  public static get COUNTRY_BY_CODE(): string {
-    return `${ApiConstants.getBaseHost()}/countries/v5/code`;
-  }
+  public static readonly BASE_URL = '/countries/v5';
+  public static readonly GET_ALL_COUNTRIES = '/countries/v5';
+  public static readonly COUNTRIES_BY_REGION = '/countries/v5/region/';
+  public static readonly COUNTRY_DATA = '/countries/v5/codes.alpha_2/';
+  public static readonly COUNTRY_BY_ALPHA2 = '/countries/v5/codes.alpha_2/';
+  public static readonly COUNTRY_BY_ALPHA3 = '/countries/v5/codes.alpha_3/';
+  public static readonly COUNTRY_BY_CODE = '/countries/v5/code';
 }
--- src/app/utility/auth.interceptor.ts
+++ src/app/utility/auth.interceptor.ts
@@ -6,8 +6,8 @@ import { ApiConstants } from '../constants/api-constants';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // If request is directed to restcountries.com and doesn't already have Authorization header
-    if (req.url.includes('restcountries.com') && !req.headers.has('Authorization')) {
+    // If request is directed to REST Countries API (direct or proxied) and doesn't already have Authorization header
+    if ((req.url.includes('/countries/v5') || req.url.includes('restcountries.com')) && !req.headers.has('Authorization')) {
       const authReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${ApiConstants.API_KEY}`
